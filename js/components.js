0a03377464b41ddc80db5b791ee7cf8ef94cac0c
Kartones <diego@kartones.net>
2022-05-06 08:15:42 +0200

    feat: game search using nice-select2

--- js/components.js
+++ js/components.js
@@ -29,6 +29,18 @@ function titleSuffixOf(desiredFilter, currentfilter, currentState) {
   }
 }
 
+export function fillSearchComponent(content) {
+  return content.replaceAll(
+    "{{js-game-search}}",
+    `<select id="gameSearch">
+    <option value="-1"> </option>
+    ${Object.values(appData.games)
+      .map((game) => `<option value="${game.id}">${game.name}</option>`)
+      .join("")}
+    </select>`
+  );
+}
+
 export function fillPlatformName(content, platformId) {
   return content.replaceAll(
     "{{js-platform-name}}",
